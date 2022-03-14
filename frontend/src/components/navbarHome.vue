56d90ed869cdc01d4fb912a6daed11307ea7f768
Balsseù Zoghbi <zoghbibalssem@gmail.com>
2022-03-14 03:16:48 +0100

    done

--- frontend/src/components/navbarHome.vue
+++ frontend/src/components/navbarHome.vue
@@ -1,27 +1,46 @@
 <template>
-  <nav
-    class="nav justify-content-center"
-    style="
-      background-color: #c6deed;
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    "
-  >
-    <ul class="nav">
-      <img src="../assets/sacem.png" />
-
-      <li class="nav-item">
-        <router-link class="nav-link active" aria-current="page" to="/"
+ <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="sacemindustries.com"><img src="../assets/sacem.png" width="100px" alt="sacemindustries" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item">
+                                  <router-link class="nav-link " aria-current="page" to="/"
           >Projet</router-link
         >
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/projet/create">Crée</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/about">Importer</router-link>
-      </li>
-    </ul>
-  </nav>
+                              </li>
+                              <li class="nav-item">
+                                 <router-link class="nav-link" to="/projet/create">Crée</router-link>
+                              </li>
+                              <li class="nav-item">
+                                       <router-link class="nav-link" to="/about">Importer</router-link>
+
+                              </li>
+                           </ul>
+                           <div class="Call"><a href="#"> <span class="yellow">logout</span></a></div>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
 </template>
 <script>
 export default {
@@ -29,11 +48,185 @@ export default {
 };
 </script>
 <style scoped>
-img {
-  position: relative;
-  margin-bottom: 3%;
-  right: 113%;
-  width: 30%;
-  height: 96%;
+/* *::after,
+*::before {
+     -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+     box-sizing: border-box;
 }
+.header {
+     width: 100%;
+     background: #eae9e4;
+     padding: 20px 30px;
+}
+
+.logo a {
+     font-size: 40px;
+     font-weight: bold;
+     text-transform: uppercase;
+     color: #fff;
+     line-height: 40px;
+}
+img {
+     max-width: 100%;
+     height: auto;
+}
+.navigation.navbar {
+     padding: 0px;
+}
+
+.navigation.navbar-dark .navbar-nav .nav-link {
+     padding:0 50px;
+     color: #3e93c3;
+     font-size: 16px;
+     line-height: 20px;
+     font-weight: 500;
+     text-transform: uppercase;
+}
+
+.navbar-dark .navbar-nav .nav-link:focus,
+.navbar-dark .navbar-nav .nav-link:hover {
+     color: #5a2f27 !important;
+}
+
+.navbar-expand-md .navbar-nav {
+     padding-right: 10px;
+}
+
+.Call a {
+     display: inline-block;
+     padding: 7px 0;
+     color: #5a2f27;
+     font-size: 20px;
+}
+
+.yellow {
+     color: #3e93c3;
+     font-weight: bold;
+     margin-left: 90%;
+          text-align: left;
+
+}
+a {
+     color: #1f1f1f;
+     text-decoration: none !important;
+     outline: none !important;
+     -webkit-transition: all .3s ease-in-out;
+     -moz-transition: all .3s ease-in-out;
+     -ms-transition: all .3s ease-in-out;
+     -o-transition: all .3s ease-in-out;
+     transition: all .3s ease-in-out;
+}
+
+a {
+     color: #222222;
+     text-decoration: none;
+     outline: none !important;
+}
+
+a,
+.btn {
+     text-decoration: none !important;
+     outline: none !important;
+     -webkit-transition: all .3s ease-in-out;
+     -moz-transition: all .3s ease-in-out;
+     -ms-transition: all .3s ease-in-out;
+     -o-transition: all .3s ease-in-out;
+     transition: all .3s ease-in-out;
+}
+@media (min-width: 768px) and (max-width: 991px) {
+    .navigation.navbar-dark .navbar-nav .nav-link {
+        padding: 0px 10px;
+    }
+    .Call a {
+        font-size: 18px;
+    }}
+    @media (min-width: 576px) and (max-width: 767px) {
+    .header {
+        padding: 15px 30px 10px 30px;
+    }
+       .navigation.navbar {
+        float: right;
+        display: inherit !important;
+        padding: 0;
+        width: 100%;
+    }
+    .navigation .navbar-collapse {
+        background: #3e93c3;
+        padding: 20px;
+        margin-top: 52px;
+        position: absolute;
+        width: 100%;
+        margin-right: 20px;
+        z-index: 999;
+    }
+    .navigation.navbar-dark .navbar-nav .nav-link {
+        padding: 10px 0;
+        color: #fff;
+    }
+    .navbar-dark .navbar-nav .nav-link:focus,
+    .navbar-dark .navbar-nav .nav-link:hover {
+        color: #eda911 !important;
+    }
+    .navigation.navbar-dark .navbar-toggler {
+        border: inherit;
+        float: right;
+        padding-top: 6px;
+    }
+
+    .carousel-control-next-icon,
+    .carousel-control-prev-icon {
+        display: none;
+    }
+    .Call {
+        display: none;
+    }
+    }
+    @media (max-width: 575px) {
+    .header {
+        padding: 30px 0px 15px 0px;
+    }
+    .logo {
+        text-align: center !important;
+        padding-bottom: 6px;
+        float: left;
+    }
+   
+    .Call {
+        display: none;
+    }
+
+    .navigation.navbar {
+        float: right;
+        display: inherit !important;
+        padding: 0;
+        width: 100%;
+        margin-top: -45px;
+    }
+    .navigation .navbar-collapse {
+        background: #3e93c3;
+        padding: 20px;
+        margin-top: 62px;
+        position: absolute;
+        width: 100%;
+        margin-right: 20px;
+        z-index: 999;
+    }
+    .navigation.navbar-dark .navbar-nav .nav-link {
+        padding: 10px 0;
+        color: #fff;
+    }
+    .navigation.navbar-dark .navbar-toggler {
+        float: right;
+        margin-right: 2px;
+        border: inherit;
+    }
+    .navbar-dark .navbar-nav .nav-link:focus,
+    .navbar-dark .navbar-nav .nav-link:hover {
+        color: #eda911 !important;
+    }
+   
+
+} */
+
 </style>
--- frontend/src/views/projet.vue
+++ frontend/src/views/projet.vue
@@ -1,24 +1,45 @@
 <template>
   <div>
     <navbarHome />
-        <div class="food">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="titlepage">
-                     <form class="d-flex" style="maring-left: -6%">
-                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="search" v-model="query" aria-describedby="basic-addon2"/>
-                      </form>
-                  </div>
-               </div>
-            </div>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-               <div class="col" v-for="projet in filtered" :key="projet.id" :projet="projet">
-                  <div class="food_box">
-                     <i><img src="../assets/trans.png" alt="#"/></i>
-                     <router-link class="nav-link" :to="'/projet/' + projet.id"><h4>{{ projet.reference }}</h4> </router-link>
-                     <br>
-                      <!-- <button class="btn">
+    <div class="home">
+      <div class="container event py-5" style="margin-left: 10%">
+        <form class="d-flex" style="maring-left: -6%">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            id="search"
+            v-model="query"
+            aria-describedby="basic-addon2"
+          />
+          <!-- <input type="submit" class="btn btn-primary" value="search" > -->
+          <!-- <span class="input-group-text" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg></span> -->
+        </form>
+        <div v-for="projet in filtered" :key="projet.id" :projet="projet">
+          <div class="projet-card card">
+            <div class="card-body">
+              <router-link class="nav-link" :to="'/projet/' + projet.id">
+                <p style="margin-top: -4%">{{ projet.reference }}</p>
+              </router-link>
+              <div class="button" style="margin-top: -7%">
+                <button class="btn" v-on:click="deleteprojet(projet.id)">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="35"
+                    height="35"
+                    fill="currentColor"
+                    class="bi bi-trash-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+                    />
+                  </svg>
+                </button>
+                <button class="btn">
                   <router-link
                     class="nav-link"
                     :to="'/projet/update/' + projet.id"
@@ -40,30 +61,13 @@
                       />
                     </svg>
                   </router-link>
-                </button> -->
-                     <button class="btn btn-outline-light"  ><router-link :to="'/projet/update/' + projet.id" >edit</router-link></button>
-                     <button class="btn btn-outline-light" v-on:click="deleteprojet(projet.id)">delete</button> 
-                <!-- <button class="btn" v-on:click="deleteprojet(projet.id)">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="35"
-                    height="35"
-                    fill="currentColor"
-                    class="bi bi-trash-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
-                    />
-                  </svg>
-                </button> -->
-                
-                  </div>
-               </div>
+                </button>
+              </div>
             </div>
-         </div>
-      </div>
-      
+          </div>
+        </div>
+        <br />
+
         <nav aria-label="Page navigation example">
           <ul class="pagination">
             <li class="page-item">
@@ -91,29 +95,9 @@
             </li>
           </ul>
         </nav>
-      <!-- end food -->
-      <!-- works -->
-      <footer>
-         <div class="footer">
-            <div class="container">
-               <!-- <div class="row">
-                  <div class="col-md-8 offset-md-2">
-                  
-                  </div>
-               </div> -->
-            </div>
-            <div class="copyright">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-md-12">
-                        <p>Copyright 2019 All Right Reserved By <a href="https://html.design/"> Free Html Templates</a></p>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -172,14 +156,45 @@ export default {
 };
 </script>
 <style scoped>
+.event {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+}
+.projet-card {
+  margin-top: 20px;
+  padding: 20px;
+  cursor: pointer;
+  border: 1px solid black;
+  background: #ffffff;
+  box-shadow: 0px 1px 19px rgba(198, 222, 237, 0.25);
+  border-radius: 40px;
+  width: 90%;
+  /* left: 175px; */
+  /* top: 12%; */
+  height: 50px;
+}
+.projet-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 1, 0);
+}
 
-
+.button {
+  /* display:inline-block; */
+  /* text-align: justify; */
+  margin-top: -8%;
+  margin-left: 80%;
+  border-radius: 20px;
+  /* display: inline; */
+}
 #search {
   background: rgba(196, 196, 196, 0.28);
   box-shadow: 0px 7px 4px rgba(58, 103, 218, 0.25);
   border-radius: 37px;
   width: 70%;
-  margin-left: 12%;
+  margin-left: 10%;
 }
-
+button {
+  color: #a2cfeb !important;
+}
 </style>
