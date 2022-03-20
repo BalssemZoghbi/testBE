698ef8d234aba8165d380d35122a9c70828cd954
eyabenaouicha <eyabenaouicha25@gmail.com>
2022-03-20 12:42:33 +0100

    about

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div class="about">
     <h1>This is an about projet {{ projet.id }}</h1>
@@ -6,16 +7,20 @@
     <h3>client : {{ projet.client }}</h3>
     <h3>elaborateur : {{ projet.elaborateur }}</h3>
     <h3>reference Client : {{ projet.refClient }}</h3>
-    <h1>user {{ this.user.id }} </h1>
-    <h3>name : {{ this.name }}</h3>
-    <h3>email  : {{ this.$store.state.user.email }}</h3>
-    <h3>password : {{ this.$store.state.user.password }}</h3>
+    <h3>temperatureMax : {{ projet.temperatureMax }}</h3>
+    <h3>attitudeMax : {{ projet.attitudeMax }}</h3>
+    <h3>type : {{ projet.type }}</h3>
+    <h3>remplissage : {{ projet.remplissage }}</h3>
+    <h3>installation : {{ projet.installation }}</h3>
+    <h3>echangeurs : {{ projet.echangeurs }}</h3>
+    <h3>dielectrique : {{ projet.dielectrique }}</h3>
+    <h3>fonctionnement : {{ projet.fonctionnement }}</h3>
+    <h3>refroidissement : {{ projet.refroidissement }}</h3>
   </div>
 </template>
 
 <script>
 import ProjetService from "@/services/ProjetService.js";
-// import {mapState} from 'vuex';
 export default {
   name: "about",
   data() {
@@ -25,16 +30,13 @@ export default {
       }
     };
   },
-  computed:{
-    
-  },
   created() {
     ProjetService.getProjetById(this.$route.params.id).then((resp) => {
       this.projet = resp.data.data;
       console.log(this.projet);
     });
   },
-  mounted:function(){
+    mounted:function(){
     console.log(this.$store.state.user);
     if(this.$store.state.user.id==-1){
       this.$router.push('/login');
