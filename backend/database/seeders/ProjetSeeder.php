1f90f0228827a1277d997476894ed1c4d9593d41
BalssemZoghbi <zoghbibalssem@gmail.com>
2022-03-24 20:35:03 +0100

    done

--- backend/database/seeders/ProjetSeeder.php
+++ backend/database/seeders/ProjetSeeder.php
@@ -14,6 +14,6 @@ class ProjetSeeder extends Seeder
      */
     public function run()
     {
-        Projet::factory()->count(10)->create();
+        // Projet::factory()->count(10)->create();
     }
 }
