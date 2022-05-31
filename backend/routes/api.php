e03c41c5f468dab9368eb4261461c851671ebcc6
BalssemZoghbi <zoghbibalssem@gmail.com>
2022-05-31 17:26:15 +0200

    consommation export json

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -57,7 +57,7 @@ Route::get('/userInactive/{id}', [UserController::class,'showinactive']);
 Route::delete('/userInactive/delete/{id}', [UserController::class,'deleteinactive']);
 
 Route::get('/projets',[ProjetController::class ,'getProjets']);
-Route::get('/allprojets/{id}',[ProjetController::class ,'exportJson']);
+Route::get('/json/{id}',[ProjetController::class ,'exportJson']);
 Route::get('/projets/search/{query}',[ProjetController::class ,'getsearch']);
 Route::get('/projets/pagination',[ProjetController::class ,'paginateProjets']);
 Route::post('/projets/add',[ProjetController::class ,'addProjet']);
